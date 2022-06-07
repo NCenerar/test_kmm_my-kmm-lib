375cecee9f48eafd0b97b97b98caec9fd745da18
Nicolas Cenerario <nicolas.cenerario@outlook.fr>
2022-06-07 07:26:16 +0200

    feat: Create a Kotlin Multi Mobile Library

---
+++ shared/src/commonMain/kotlin/io/github/ncenerar/test/mykmmlibrary/Greeting.kt
@@ -0,0 +1,7 @@
+package io.github.ncenerar.test.mykmmlibrary
+
+class Greeting {
+    fun greeting(): String {
+        return "Hello, ${Platform().platform}!"
+    }
+}
---
+++ shared/src/commonMain/kotlin/io/github/ncenerar/test/mykmmlibrary/Platform.kt
@@ -0,0 +1,5 @@
+package io.github.ncenerar.test.mykmmlibrary
+
+expect class Platform() {
+    val platform: String
+}
